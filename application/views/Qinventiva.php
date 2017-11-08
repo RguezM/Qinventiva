bb45995ab42e45ab5c9a2b24d1ae3070c538d407
RguezM <miguel.rodriguez.developer@gmail.com>
2017-11-07 19:06:22 -0600

    Inicio del proyecto Qinventiva

---
+++ application/views/Qinventiva.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+	<html lang="en">
+
+		<head>
+
+		    <meta charset="utf-8">
+		    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+		    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		    <meta name="author" content="Miguel Angel Rodriguez Torres">
+		    <meta name="author" content="fede">
+		    <meta name="author" content="langa">
+
+		    <title>Qinventiva</title>
+		    <link href="Assets\css/bootstrap.min.css" rel="stylesheet">
+		    <link href="Assets\css/business-frontpage.css" rel="stylesheet">
+
+		</head>
+
+		<body>
+			<h3>holis</h3>
+		</body>
+
+	</html>
+
+
